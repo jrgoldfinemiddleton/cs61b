da259ed10e5ac49d575ceb47400614a2eda123d3
Jason Goldfine-Middleton <jrgoldfinemiddleton@gmail.com>
2014-09-21 22:28:50 -0700

    Completed lab1 and ignored binaries in bin/ directory

--- src/lab1/Names.java
+++ src/lab1/Names.java
@@ -1,6 +1,5 @@
 /* Names.java */
-
-import java.io.*;
+package lab1;
 
 /** The Names class provides a single function, main, that will
  *   perform various manipulations of the name John Fitzgerald Kennedy. 
@@ -25,18 +24,18 @@ class Names {
     initials = firstInit.concat(middleInit);
     initials = initials.concat(lastInit);
 
-    System.out.println()
+    System.out.println();
     System.out.println(first + " " + middle + " " + last + " ");
     System.out.println(initials);
     System.out.println(last + ", " + first + " " + middle);
     System.out.println(last + ", " + first + " " + middleInit +".");
-    System.out.println(first.toUpperCase() + " " + last.toUpperCase);
+    System.out.println(first.toUpperCase() + " " + last.toUpperCase());
 
     System.out.println(first + " equals john is " + first.equals("john"));
     System.out.println(first + " equals john (ignoring case) is " 
 		       + first.equalsIgnoreCase("john"));
     System.out.println("The character at index 3 in " + middle + " is " +
-		       middle.substring(3,1));
+		       middle.substring(3,4));
     System.out.println("The index of \"gerald\" within " + middle + " is " +
 		       middle.indexOf("gerald"));
     System.out.println("The index of \"gerald\" within " + last + " is " +
