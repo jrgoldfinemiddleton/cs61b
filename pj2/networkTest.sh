f5d52d59201544e506e887708231448468b89824
Jason Goldfine-Middleton <jrgoldfinemiddleton@gmail.com>
2014-10-22 11:24:05 -0700

    Improved pj2 script

--- pj2/networkTest.sh
+++ pj2/networkTest.sh
@@ -1,11 +1,6 @@
-#!/bin/sh
-START=$(($(date +'%s * 1000 + %-N / 1000000')))
-
+#! /usr/bin/env bash
 for ((i = 0; i < 10; i++)); do
   java Network -q machine random
 done
 
-END=$(($(date +'%s * 1000 + %-N / 1000000')))
-TOTAL=`expr $END - $START`
-TOTAL=`expr $TOTAL / 1000`
-echo $TOTAL
+echo $SECONDS
